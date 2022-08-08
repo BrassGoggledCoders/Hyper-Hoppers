3c409354d7f1f4431707a8e417ae7ed79ee49feb
Skylar <skylar.som@gmail.com>
2022-08-07 22:05:29 -0400

    Semi proper shape

--- src/main/java/xyz/brassgoggledcoders/hyperhoppers/block/PassductBlock.java
+++ src/main/java/xyz/brassgoggledcoders/hyperhoppers/block/PassductBlock.java
@@ -1,5 +1,8 @@
 package xyz.brassgoggledcoders.hyperhoppers.block;
 
+import com.google.common.collect.HashBasedTable;
+import com.google.common.collect.Table;
+import net.minecraft.Util;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
 import net.minecraft.server.level.ServerLevel;
@@ -13,18 +16,42 @@ import net.minecraft.world.level.block.state.StateDefinition;
 import net.minecraft.world.level.block.state.properties.BlockStateProperties;
 import net.minecraft.world.level.block.state.properties.EnumProperty;
 import net.minecraft.world.level.block.state.properties.Property;
-import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraft.world.phys.shapes.CollisionContext;
+import net.minecraft.world.phys.shapes.Shapes;
+import net.minecraft.world.phys.shapes.VoxelShape;
+import net.minecraftforge.items.ItemHandlerHelper;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 import xyz.brassgoggledcoders.hyperhoppers.blockentity.PassductBlockEntity;
 import xyz.brassgoggledcoders.hyperhoppers.content.HyppersBlocks;
 
 import javax.annotation.ParametersAreNonnullByDefault;
+import java.util.EnumMap;
+import java.util.Objects;
 import java.util.Random;
 
 public class PassductBlock extends Block implements EntityBlock {
     public static final Property<Direction> FACING = BlockStateProperties.FACING;
     public static final Property<Direction> SPOUT = EnumProperty.create("spout", Direction.class);
+
+    private static final Table<Direction, Direction, VoxelShape> VOXEL_SHAPE_TABLE = HashBasedTable.create();
+    private static final EnumMap<Direction, VoxelShape> FACING_SHAPE = Util.make(new EnumMap<>(Direction.class), map -> {
+        map.put(Direction.DOWN, Block.box(3.0D, 0.0D, 3.0D, 13.0D, 8.0D, 13.0D));
+        map.put(Direction.UP, Block.box(3.0D, 8.0D, 3.0D, 13.0D, 16.0D, 13.0D));
+        map.put(Direction.NORTH, Block.box(3.0D, 3.0D, 0.0D, 13.0D, 13.0D, 8.0D));
+        map.put(Direction.SOUTH, Block.box(3.0D, 3.0D, 8.0D, 13.0D, 13.0D, 16.0D));
+        map.put(Direction.EAST, Block.box(8.0D, 3.0D, 3.0D, 16.0D, 13.0D, 13.0D));
+        map.put(Direction.WEST, Block.box(0.0D, 3.0D, 3.0D, 8.0D, 13.0D, 13.0D));
+    });
+    private static final EnumMap<Direction, VoxelShape> SPOUT_SHAPE = Util.make(new EnumMap<>(Direction.class), map -> {
+        map.put(Direction.DOWN, Block.box(5.0D, 0.0D, 5.0D, 11.0D, 8.0D, 11.0D));
+        map.put(Direction.UP, Block.box(5.0D, 8.0D, 5.0D, 11.0D, 16.0D, 11.0D));
+        map.put(Direction.NORTH, Block.box(5.0D, 5.0D, 0.0D, 11.0D, 11.0D, 8.0D));
+        map.put(Direction.SOUTH, Block.box(5.0D, 5.0D, 8.0D, 11.0D, 11.0D, 16.0D));
+        map.put(Direction.EAST, Block.box(8.0D, 5.0D, 5.0D, 16.0D, 11.0D, 11.0D));
+        map.put(Direction.WEST, Block.box(0.0D, 5.0D, 5.0D, 8.0D, 11.0D, 11.0D));
+
+    });
 
     private final int slots;
     private final int maxAttempts;
@@ -40,6 +67,21 @@ public class PassductBlock extends Block implements EntityBlock {
                 .setValue(FACING, Direction.UP)
                 .setValue(SPOUT, Direction.DOWN)
         );
+    }
+
+    @Override
+    @NotNull
+    @ParametersAreNonnullByDefault
+    @SuppressWarnings("deprecation")
+    public VoxelShape getShape(BlockState pState, BlockGetter pLevel, BlockPos pPos, CollisionContext pContext) {
+        Direction facing = pState.getValue(FACING);
+        Direction spout = pState.getValue(SPOUT);
+
+        if (!VOXEL_SHAPE_TABLE.contains(facing, spout)) {
+            VOXEL_SHAPE_TABLE.put(facing, spout, Shapes.or(FACING_SHAPE.get(facing), SPOUT_SHAPE.get(spout)));
+        }
+
+        return Objects.requireNonNull(VOXEL_SHAPE_TABLE.get(facing, spout));
     }
 
     @Override
